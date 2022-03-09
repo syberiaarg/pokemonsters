1978ecf5d4a2f7d97c3e7c905e6b1cef9636a6a1
syberiaarg <syberiaarg@gmail.com>
2022-03-09 16:50:10 -0300

    Improving Style

--- src/Components/Visualizer/Pokemon/Pokemon.js
+++ src/Components/Visualizer/Pokemon/Pokemon.js
@@ -1,53 +1,49 @@
 import React, { useEffect, useState } from "react";
+import { firstLetterUpper } from "src/utils";
 import "./Pokemon.css";
 
+const initialPokemon = {
+  id: 0,
+  name: "",
+  types: [],
+  weight: 0,
+  height: 0,
+  image: "",
+};
+
 const Pokemon = ({ name, url }) => {
-  const [pokeImage, setPokeImage] = useState("");
-  const [pokeType, setPokeType] = useState([]);
-  const [pokeWeight, setPokeWeight] = useState("");
-  const [pokeHeight, setPokeHeight] = useState("");
-  const [pokeID, setPokeID] = useState("");
-
-  const nameLabelFix = ({ name }) => {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-  };
-
-  const typeLabelFix = (types) => {
-    return types.charAt(0).toUpperCase() + types.slice(1);
-  };
+  const [pokemon, setPokemon] = useState(initialPokemon);
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setPokeImage(data.sprites.front_default);
-        setPokeType(data.types);
-        setPokeWeight(data.weight);
-        setPokeHeight(data.height);
-        setPokeID(data.id);
+        setPokemon({
+          id: data.id,
+          name: name,
+          types: data.types,
+          weight: data.weight,
+          height: data.height,
+          image: data.sprites.front_default,
+        });
       });
   }, []);
 
   return (
     <div className="pokeData">
-      <div className="pokeName">
-        <label>{nameLabelFix({ name })}</label> <label>ID: {pokeID}</label>
-      </div>
-      <div className="pokeType">
-        {pokeType.map((types) => (
-          <div>
-            <label key={types["type"]["name"]}>
-              {typeLabelFix(types["type"]["name"])}
-            </label>
-          </div>
-        ))}
-      </div>
-      <div className="pokeForm">
-        <label key={pokeWeight}>Weight: {pokeWeight}</label>
-        <label key={pokeHeight}>Height: {pokeHeight}</label>
+      <div className="pokeInfo">
+        <label>{firstLetterUpper(name)}</label>
+        <label>ID: {pokemon.id}</label>
+        <div className="pokeType">
+          {pokemon.types.map(({ type }) => (
+            <label key={type.name}>{firstLetterUpper(type.name)}</label>
+          ))}
+        </div>
+        <label>Weight: {pokemon.weight}</label>
+        <label>Height: {pokemon.height}</label>
       </div>
       <div className="pokeSprite">
-        <img alt="Sprite" src={pokeImage} />
+        <img alt="Sprite" src={pokemon.image} />
       </div>
     </div>
   );
