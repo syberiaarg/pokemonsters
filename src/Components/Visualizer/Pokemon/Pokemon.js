b2c303f29c2e2fa24604580c049aabd101c5dd86
Santiago Gimenez <syberiaarg@gmail.com>
2022-03-27 19:45:05 -0300

    Updated Funcionality

--- src/Components/Visualizer/Pokemon/Pokemon.js
+++ src/Components/Visualizer/Pokemon/Pokemon.js
@@ -8,11 +8,14 @@ const initialPokemon = {
   types: [],
   weight: 0,
   height: 0,
+  sprite: "",
   image: "",
+  stats: "",
 };
 
 const Pokemon = ({ name, url }) => {
   const [pokemon, setPokemon] = useState(initialPokemon);
+  const [completeData, showCompleteData] = useState(false);
 
   useEffect(() => {
     fetch(url)
@@ -24,20 +27,23 @@ const Pokemon = ({ name, url }) => {
           types: data.types,
           weight: data.weight,
           height: data.height,
-          image: data.sprites.front_default,
+          sprite: data.sprites.front_default,
+          altsprite: data.sprites.back_default,
+          image: data.sprites.other.dream_world.front_default,
+          stats: data.stats,
         });
       });
   }, []);
 
   return (
-    <div className="pokeData">
+    <div className="pokeData" onClick={() => showCompleteData(!completeData)}>
       <div className="pokeInfo">
         <div className="name">
           <label>
             <strong>{firstLetterUpper(name)}</strong>
           </label>
         </div>
-        <label>ID: {pokemon.id}</label>
+        <label>N°: {pokemon.id}</label>
         <div className="pokeType">
           {pokemon.types.map(({ type }) => (
             <div
@@ -52,7 +58,15 @@ const Pokemon = ({ name, url }) => {
         <label>Height: {pokemon.height}</label>
       </div>
       <div className="pokeSprite">
-        <img alt="Sprite" src={pokemon.image} />
+        <img alt="Sprite" src={pokemon.sprite} />
+      </div>
+      <div className="moreInfo">
+        {completeData && (
+          <div className="images">
+            <img alt="altSprite" src={pokemon.altsprite} />
+            <img alt="otherImage" src={pokemon.image} />
+          </div>
+        )}
       </div>
     </div>
   );
