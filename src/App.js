d352c4bfab3324c2e1d63245f61e7d51c4a74124
Syberia <syberiaarg@gmail.com>
2022-08-26 12:09:05 -0300

    Navbar Funcionality Updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./Components/Home"
 import AboutMe from "./Pages/AboutMe"
 import Registry from "./Pages/Registry"
 import Team from "./Pages/Team"
+import ResponsiveAppBar from "./Components/Navbar/";
 
 
 function App() {
 
   return (
     <div className="App">
+      <header>
+        <ResponsiveAppBar />
+      </header>
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/registry" element={<Registry />} />
