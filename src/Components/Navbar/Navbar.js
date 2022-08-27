99a922e40412d8026c674e5ab039eb224b0da5be
Syberia <syberiaarg@gmail.com>
2022-08-27 05:24:44 -0300

    Updated Registry Page and Optimized Components Style

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
@@ -38,7 +38,7 @@ const ResponsiveAppBar = () => {
                                 fontFamily: 'monospace',
                                 fontWeight: 700,
                                 letterSpacing: '.3rem',
-                                color: 'inherit',
+                                color: 'white',
                                 textDecoration: 'none',
                             }}
                         >
--- src/Pages/Registry/Registry.js
+++ src/Pages/Registry/Registry.js
@@ -1,8 +1,115 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './Registry.css'
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 
-const Registry = () => (
-    <div>
-        Registry
-    </div>
-)
+
+const Copyright = (props) => {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="/">
+                Pokemonsters
+            </Link>{' '}
+            {new Date().getFullYear()}
+        </Typography>
+    );
+}
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+
+const Registry = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+        });
+    };
+
+    return (
+        <div className='teamForm'>
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xl">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 3, bgcolor: 'primary.main', width: 48, height: 48 }}>
+                            <CatchingPokemonIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h3">
+                            GRAB YOUR TEAM
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                GET THEM
+                            </Button>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
+        </div>
+    );
+}
+
 
 export default Registry
